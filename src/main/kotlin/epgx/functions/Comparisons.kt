3941493d3d4f0d8951823ce86693c0ec843bbbf5
Hamza <muhammadhamza1311@gmail.com>
2020-05-28 21:39:48 +0500

    comparisons: @@: fix param name

--- src/main/kotlin/epgx/functions/Comparisons.kt
+++ src/main/kotlin/epgx/functions/Comparisons.kt
@@ -5,5 +5,5 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.CustomFunction
 
 @Suppress("FunctionName")
-infix fun <T> Column<T>.`@@`(TsQuery: CustomFunction<*>): AtAtOp =
-        AtAtOp(this, TsQuery)
+infix fun <T> Column<T>.`@@`(tsQuery: CustomFunction<*>): AtAtOp =
+        AtAtOp(this, tsQuery)
